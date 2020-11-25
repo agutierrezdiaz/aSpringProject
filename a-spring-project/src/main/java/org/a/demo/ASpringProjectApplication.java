63020ba8e31ac823a73a0d9656c50eea546698bc
agutierrezdiaz <agutierrezdiaz90@gmail.com>
2020-11-25 20:45:00 +0100

    Empty project

---
+++ a-spring-project/src/main/java/org/a/demo/ASpringProjectApplication.java
@@ -0,0 +1,13 @@
+package org.a.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ASpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ASpringProjectApplication.class, args);
+	}
+
+}
---
+++ a-spring-project/src/test/java/org/a/demo/ASpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.a.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ASpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
